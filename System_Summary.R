5db4ede01747b88b4b84846cb104d4a31452da5c
greglenane <61029235+greglenane@users.noreply.github.com>
2020-08-10 14:20:17 -0400

    Update System_Summary.R

--- System_Summary.R
+++ System_Summary.R
@@ -17,19 +17,11 @@ System_Summary_List <- lapply(System_Preprocess, function(x)Source_Summary(x))
 #bind all summary tables into system summary
 System_Summary = do.call("rbind",System_Summary_List)
 #Save System Summary table
-setwd(dir)
+setwd("J:/deans/Presidents/SixSigma/MSHS Productivity/Productivity/Analysis/FTE Projections Dashboard/System Summary")
 saveRDS(System_Summary,file = "System_Summary.rds")
 #########################################################################################
 
 ##Reload Master##########################################################################
-setwd(dir)
+setwd("J:/deans/Presidents/SixSigma/MSHS Productivity/Productivity/Analysis/FTE Projections Dashboard/System Summary")
 System_Summary <- readRDS("System_Summary.rds")
 #########################################################################################
-
-# #Savey PP Summary tables as excel files
-# setwd("C:/Users/gregl/OneDrive/Documents/MSH-MSQ-Payroll/Summary Tables")
-# file <- paste0("MSH_MSQ_PP HOURS_",Sys.Date(),".xlsx")
-# hs <- createStyle(textDecoration = "BOLD", fgFill = "#B2B3B2", fontColour = "#000000", halign = "center", border = "TopBottomLeftRight", borderColour = "#000000")
-# df <- Summary_Hours_PP
-# write.xlsx(df, file=file, startCol = c(1,1), startRow = c(1,1),
-#            asTable = c(F,F), headerStyle = hs, borders = "all")
