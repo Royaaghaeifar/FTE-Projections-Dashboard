f57c2ffbe78ddce9bc64fee56dc1ba2cff67be77
greglenane <gregory.lenane@mssm.edu>
2020-08-27 14:33:31 -0400

    Fixing Directories for System_Summary.R

--- System_Summary.R
+++ System_Summary.R
@@ -1,14 +1,14 @@
 
-dir <- "J:/deans/Presidents/SixSigma/MSHS Productivity/Productivity/Analysis/FEMA Reimbursement/MSHS-FEMA-Reimbursement/"
-setwd(dir)
+#dir <- "J:/deans/Presidents/SixSigma/MSHS Productivity/Productivity/Analysis/FEMA Reimbursement/MSHS-FEMA-Reimbursement/"
+
 
 ##Refresh Master#########################################################################
-setwd(paste0(dir,"Preprocess"))
-folder_preprocess <- paste0(dir,"Preprocess")
+#setwd(paste0(getwd(),"/Preprocess"))
+#folder_preprocess <- getwd()
 #create list of preprocessed files
-preprocess <- list.files(path=folder_preprocess)
+preprocess <- list.files(path=paste0(getwd(),"/Preprocess"))
 #bring in functions to run preprocess scripts and summarize them
-source(paste0(dir,"Source_Summary_FTE Trend.R"))
+source("Source_Summary_FTE Trend.R")
 #Run source function
 System_Source <- lapply(preprocess,function(x)Source_Func(x))
 System_Preprocess <- list(MSH_MSQ = data_MSH_MSQ,MSBIB = data_MSBI_MSB,MSSLW = data_MSSL_MSW,MSH_MSQ_Oracle = data_MSH_MSQ_oracle)
@@ -19,6 +19,7 @@ System_Summary = do.call("rbind",System_Summary_List)
 #Save System Summary table
 setwd("J:/deans/Presidents/SixSigma/MSHS Productivity/Productivity/Analysis/FTE Projections Dashboard/System Summary")
 saveRDS(System_Summary,file = "System_Summary.rds")
+
 #########################################################################################
 
 ##Reload Master##########################################################################
