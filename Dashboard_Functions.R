1335d9811ef27e55a73d098825e940ba4b8b44c6
greglenane <gregory.lenane@mssm.edu>
2020-09-17 11:25:13 -0400

    Edits to ktable and service line graph functions

--- Dashboard_Functions.R
+++ Dashboard_Functions.R
@@ -1,16 +1,30 @@
-```{r, echo=F,warning=F,message=F,results='hide'}
 library(dplyr)
+library(tidyr)
 library(ggplot2)
 library(plotly)
+library(knitr)
+library(kableExtra)
 
 ##Reload Master##########################################################################
 System_Summary <- readRDS("J:/deans/Presidents/SixSigma/MSHS Productivity/Productivity/Analysis/FTE Projections Dashboard/System Summary/System_Summary.rds")
 System_Summary <- System_Summary %>%
-  filter(PP.END.DATE >= as.Date("12/22/2019",format="%m/%d/%Y"))
+  filter(PP.END.DATE >= as.Date("12/22/2019",format="%m/%d/%Y")) %>%
+  replace_na(list(SERVICE.LINE = "Other"))
 #########################################################################################
 
 #Worked hour pay code mappings
-worked <- c("REGULAR","OVERTIME","OTHER_WORKED")
+worked_paycodes <- c('REGULAR','OVERTIME','OTHER_WORKED','EDUCATION','ORIENTATION','AGENCY')
+#pre covid pay period end dates
+pre_covid_PP <- as.Date(c('2020-01-04','2020-01-18','2020-02-01','2020-02-15','2020-02-29'))
+#get unique service lines
+service_lines <- list("ICU","Labor & Delivery","Mother/Baby","Progressive","Med/Surg","Psych","RETU", "Perioperative Services","Support Services","Pharmacy","Radiology","Lab","Emergency Department","Other")
+report_period_length <- 3
+biweekly_fte <- 75
+digits_round <- 2
+
+Ktable <- as.data.frame(System_Summary)
+Suport_Service_table2 <- as.data.frame(System_Summary)
+
 #Site Based Service List
 MSH_service_list <- list("ICU","Labor & Delivery","Mother/Baby","Progressive","Med/Surg","Psych","RETU", "Perioperative Services","Support Services","Pharmacy","Radiology","Lab","Emergency Department")
 MSQ_service_list <- list("ICU","Med/Surg","Perioperative Services","Support Services","Pharmacy","Radiology","Emergency Department")
@@ -26,9 +40,9 @@ data <- System_Summary %>%
            PP.END.DATE >as.Date("5/9/2020",format="%m/%d/%Y"),   #or greater than 5/9/2020
          PROVIDER == 0, #remove providers
          INCLUDE.HOURS == 1, #only use included hour paycodes
-         PAY.CODE.MAPPING %in% worked) %>% #remove unproductive paycodes 
+         PAY.CODE.MAPPING %in% worked_paycodes) %>% #remove unproductive paycodes 
   group_by(PAYROLL,DEFINITION.CODE,DEFINITION.NAME,SERVICE.LINE,PP.END.DATE) %>%
-  summarise(FTE = round(sum(HOURS, na.rm = T)/75,2)) %>% #calculate FTE
+  summarise(FTE = round(sum(HOURS, na.rm = T)/biweekly_fte,digits_round)) %>% #calculate FTE
   mutate(DEPARTMENT = paste0(DEFINITION.CODE," - ",toupper(DEFINITION.NAME)), #capitalize department
          DATES = as.character(PP.END.DATE)) %>% #add character form of data
   arrange(PP.END.DATE) #arrange by pay period end date
@@ -95,9 +109,10 @@ service_line <- function(hosp,service){
     pivot_wider(id_cols = c("DEFINITION.CODE","DEFINITION.NAME","DEPARTMENT"),
                 names_from = "PP.END.DATE",
                 values_from = FTE) #pivot dataframe to bring in NAs for missing PP
+  data_service <- data_service[,c(1:3,(ncol(data_service)-7):ncol(data_service))]
   data_service <- data_service %>% 
     pivot_longer(cols = 4:ncol(data_service),
-                 names_to = "PP.END.DATE") #pivot dataframe to original form
+                 names_to = "PP.END.DATE")#pivot dataframe to original form
   data_service <- data_service %>% 
     mutate(FTE = case_when(
       is.na(value) ~ 0, #if FTE is NA -> 0
@@ -114,13 +129,14 @@ service_line <- function(hosp,service){
     theme(plot.title=element_text(hjust=.5,size=20),
           axis.title = element_text(face="bold"),
           legend.text=element_text(size = 6)) #create and style service line graph
-  service_line_graphly <- ggplotly(service_line_graph,
-                                   tooltip=c("group","x","y")) %>%
+  service_line_graphly <- ggplotly(service_line_graph,tooltip=c("group","x","y")) %>%
+    config(displaylogo = F,
+           modeBarButtonsToRemove = c("lasso2d","autoScale2d","select2d","toggleSpikelines")) %>%
     layout(title = list(xanchor = "center")) #turn graph into plotly interactive
   return(service_line_graphly)
 }
 
-#Kable
+#Site level Kable
 k <- function(hosp,service){
   library(tidyr)
   kdata <- data %>% 
@@ -133,7 +149,45 @@ k <- function(hosp,service){
   colnames(kdata)[1] <- "SERVICE LINE" 
   sort <- colnames(kdata)[ncol(kdata)] 
   kdata <- kdata %>% ungroup() %>% arrange(desc(!!sym(sort))) 
+  kdata$`Baseline Avg.` <- rowMeans(subset(kdata, select = c("2020-01-04","2020-01-18","2020-02-01","2020-02-15","2020-02-29"), na.rm = TRUE))
+  kdata$`Baseline Avg.` <- round(kdata$`Baseline Avg.`,digits_round)
+  kdata <- kdata[,c(1:2,(ncol(kdata)-8):ncol(kdata))]
   Ktable <<- kdata 
 }
 
-```
+#System level Kable
+premier_sum_stats <- function(sys.sum, site, serv.line){
+  #Pre Processsing Data
+  data_export <- System_Summary %>% ungroup() %>% as.data.frame() %>%
+    select(PAYROLL, SERVICE.LINE, PP.END.DATE, PAY.CODE.MAPPING, PROVIDER, HOURS,INCLUDE.HOURS) %>%
+    filter(PAYROLL == site,
+           SERVICE.LINE == serv.line,
+           PROVIDER == 0,
+           INCLUDE.HOURS == 1,
+           PAY.CODE.MAPPING %in% worked_paycodes) %>%
+    select(PAYROLL, SERVICE.LINE, PP.END.DATE, HOURS) %>%
+    arrange(desc(PP.END.DATE))
+  #Calculating Pre Covid FTE Average
+  data_pre_covid_ave <- data_export %>% filter(PP.END.DATE %in% pre_covid_PP) %>% 
+    mutate(PP.END.DATE = "Baseline Avg.", 
+           HOURS = round(HOURS/(biweekly_fte*length(pre_covid_PP)),digits_round)) %>%
+    pivot_wider(names_from = PP.END.DATE, values_from = HOURS, values_fn = list(HOURS = sum))
+  #Calculating Report Period FTEs
+  PayPeriods <- data_export %>% select(PP.END.DATE) %>% distinct() %>% slice(1:(report_period_length*2))
+  data_current <- data_export %>% 
+    filter(PP.END.DATE %in% PayPeriods$PP.END.DATE) %>% arrange(PP.END.DATE) %>%
+    mutate(HOURS = round(HOURS/biweekly_fte,digits_round),
+           PP.END.DATE = format(PP.END.DATE, '%m/%d')) %>%
+    pivot_wider(names_from = PP.END.DATE,values_from = HOURS, values_fn = list(HOURS = sum))
+  #Calculating Report Average
+  data_report_ave <- data_export %>%
+    filter(PP.END.DATE %in% PayPeriods$PP.END.DATE[1:report_period_length]) %>%
+    mutate(PP.END.DATE = 'Report Period Avg.',
+           HOURS = round(HOURS/(biweekly_fte*report_period_length),digits_round)) %>%
+    pivot_wider(names_from = PP.END.DATE,values_from = HOURS, values_fn = list(HOURS = sum))
+  #Combining and formatting
+  data_final <- left_join(data_current, data_report_ave)
+  data_final <- left_join(data_final, data_pre_covid_ave)
+  colnames(data_final)[1:2] <- c('Site', 'Service Line')
+  return(data_final)
+}
