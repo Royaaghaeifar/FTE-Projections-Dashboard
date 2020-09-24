e04b9bf43b58664059bb9d47bcad2668b9cff464
greglenane <gregory.lenane@mssm.edu>
2020-09-24 10:50:34 -0400

    Add max PP end date user input

--- Render.R
+++ Render.R
@@ -6,7 +6,9 @@ library(rmarkdown)
 source("System_Summary.R")
 
 #Save System Summary table
-saveRDS(System_Summary,file = "J:/deans/Presidents/SixSigma/MSHS Productivity/Productivity/Analysis/FTE Projections Dashboard/System Summary/System_Summary.rds")
+saveRDS(System_Summary,file="J:/deans/Presidents/SixSigma/MSHS Productivity/Productivity/Analysis/FTE Projections Dashboard/System Summary/System_Summary.rds")
 
+#Enter max PP end date for dashboard
+end <- "08/01/2020"
 #render FTE Trend Dashboard
-render(FTE_Trend_Dashboard.R)
+render("FTE_Trend_Dashboard.rmd")
