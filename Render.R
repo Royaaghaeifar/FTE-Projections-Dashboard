31e60c8bc5bbc5a951678a01d799344d37823c56
greglenane <gregory.lenane@mssm.edu>
2020-10-15 14:38:47 -0400

    refresh to 8/29/2020

--- Render.R
+++ Render.R
@@ -9,6 +9,6 @@ source("System_Summary.R")
 saveRDS(System_Summary,file="J:/deans/Presidents/SixSigma/MSHS Productivity/Productivity/Analysis/FTE Projections Dashboard/System Summary/System_Summary.rds")
 
 #Enter max PP end date for dashboard
-end <- "08/01/2020"
+end <- "08/29/2020"
 #render FTE Trend Dashboard
 render("FTE_Trend_Dashboard.rmd")
