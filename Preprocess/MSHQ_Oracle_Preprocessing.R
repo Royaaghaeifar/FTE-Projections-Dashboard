e14de5543822e3c9bafa85884c52581666436a40
greglenane <gregory.lenane@mssm.edu>
2020-10-19 10:56:49 -0400

    Use Oracle cost center if no GEAC map exists

--- Preprocess/MSHQ_Oracle_Preprocessing.R
+++ Preprocess/MSHQ_Oracle_Preprocessing.R
@@ -8,20 +8,41 @@ COA <- read.csv("J:/deans/Presidents/SixSigma/MSHS Productivity/Productivity/Ana
 #Read in job code descriptions
 JCdesc <- read.csv("J:/deans/Presidents/SixSigma/MSHS Productivity/Productivity/Analysis/FEMA Reimbursement/MSHS-FEMA-Reimbursement/Reference Tables/JCdesc.csv",header = T, stringsAsFactors = F)
 
-#Remove NA worked department
+#Replace departments that failed GEAC map
 oracle <- data_MSH_MSQ_oracle %>%
-  filter(Reverse.Map.for.Worked != "")
+  mutate(Reverse.Map.for.Worked = case_when(
+    Reverse.Map.for.Worked == "" ~ Department.IdWHERE.Worked,
+    TRUE ~ Reverse.Map.for.Worked))
+oracle <- oracle %>%
+  mutate(Reverse.Map.for.Home = case_when(
+    Reverse.Map.for.Home == "" ~ paste0(HD_COFT,HD_Location,HD_Department),
+    TRUE ~ Reverse.Map.for.Home))
 
-#Blank home department becomes worked department
-Blank <- oracle %>%
-  filter(Reverse.Map.for.Home == "") %>%
-  mutate(Reverse.Map.for.Home = Reverse.Map.for.Worked)
-oracle <- rbind(filter(oracle,Reverse.Map.for.Home != ""),Blank)
+# #Remove NA worked department
+# oracle <- data_MSH_MSQ_oracle %>%
+#   filter(Reverse.Map.for.Worked != "")
+# 
+# #Blank home department becomes worked department
+# Blank <- oracle %>%
+#   filter(Reverse.Map.for.Home == "") %>%
+#   mutate(Reverse.Map.for.Home = Reverse.Map.for.Worked)
+# oracle <- rbind(filter(oracle,Reverse.Map.for.Home != ""),Blank)
 
 #Take first 8 digits of Home and Worked department
 oracle <- oracle %>%
-  mutate(Reverse.Map.for.Worked = substr(Reverse.Map.for.Worked,1,8),
-         Reverse.Map.for.Home = substr(Reverse.Map.for.Home,1,8))
+  mutate(
+    Reverse.Map.for.Worked = 
+      case_when(
+        nchar(Reverse.Map.for.Worked) == 12 ~ substr(Reverse.Map.for.Worked,1,8),
+        TRUE ~ Reverse.Map.for.Worked),
+    Reverse.Map.for.Home = 
+      case_when(
+        nchar(Reverse.Map.for.Home) == 12 ~ substr(Reverse.Map.for.Home,1,8),
+        TRUE ~ Reverse.Map.for.Home))
+
+# oracle <- oracle %>%
+#   mutate(Reverse.Map.for.Worked = substr(Reverse.Map.for.Worked,1,8),
+#          Reverse.Map.for.Home = substr(Reverse.Map.for.Home,1,8))
 
 #Bring in department location
 oracle <- left_join(oracle,COA, by = c("Reverse.Map.for.Worked" = "Column2")) %>%
@@ -38,9 +59,9 @@ oracle <- oracle %>%
          Hours = as.numeric(Hours),
          Expense = as.numeric(Expense))
 #Column names
-colnames(oracle)[c(6,7,12:15,29,30,32,33,35:37)] = c("START.DATE","END.DATE","J.C","PAY.CODE","HOME.DESCRIPTION",
-                                               "WRKD.DESCRIPTION","DPT.WRKD","DPT.HOME","HOURS","EXPENSE",
-                                               "WRKD.LOCATION","HOME.LOCATION","J.C.DESCRIPTION")
+colnames(oracle)[c(6,7,12:15,29,30,32,33,35:37)] = c("START.DATE","END.DATE","J.C","PAY.CODE",
+                                               "HOME.DESCRIPTION","WRKD.DESCRIPTION","DPT.WRKD","DPT.HOME",
+                                               "HOURS","EXPENSE","WRKD.LOCATION","HOME.LOCATION","J.C.DESCRIPTION")
 #Only take necessary columns
 #data_MSH_MSQ_oracle <- select(oracle,c(6,7,12:15,29,30,32:37))
 data_MSH_MSQ_oracle <<- oracle
