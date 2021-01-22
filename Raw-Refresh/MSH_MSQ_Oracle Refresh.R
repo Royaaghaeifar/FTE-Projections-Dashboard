52c294c5fcc2862e8aaedef8674cfdf36dd48796
greglenane <61029235+greglenane@users.noreply.github.com>
2021-01-22 15:37:08 -0500

    Add files via upload

---
+++ Raw-Refresh/MSH_MSQ_Oracle Refresh.R
@@ -0,0 +1,47 @@
+library(dplyr)
+library(tidyr)
+
+dir <- "~/MSH-MSQ-Payroll/"
+setwd(dir)
+
+##MSHQ##
+#List files from MSQ Raw folder
+PPend_list <- list("04/25/2020","05/23/2020","06/20/2020","08/01/2020","08/29/2020","09/26/2020","10/24/2020","11/21/2020")
+setwd(paste0(dir,"MSHQ Oracle/"))
+folderOracle <- paste0(dir,"MSHQ Oracle/")     
+Oracle_file_list <- list.files(path=folderOracle, pattern="*.txt")
+#Read files in MSQ Raw as csv
+ORACLElist = lapply(Oracle_file_list, function(x)read.csv(x, sep = "~", header=T, stringsAsFactors = F,colClasses = rep("character",32)))
+#Remove overlapping dates from raw txt files
+for(i in 1:length(ORACLElist)){
+  if(i == 1){
+    ORACLElist[[i]] <- ORACLElist[[i]] %>%
+      filter(as.Date(End.Date, format = "%m/%d/%Y") <= as.Date(PPend_list[[i]], format = "%m/%d/%Y"))
+  } else {
+    ORACLElist[[i]] <- ORACLElist[[i]] %>%
+      filter(as.Date(End.Date, format = "%m/%d/%Y") <= as.Date(PPend_list[[i]], format = "%m/%d/%Y"),
+             as.Date(Start.Date, format = "%m/%d/%Y") > max(as.Date(ORACLElist[[i-1]]$End.Date,format = "%m/%d/%Y")))
+  }
+}
+#Bind all MSHQ files
+Oracle = do.call("rbind", ORACLElist)
+#Remove Duplicate rows and add worked entity column
+Oracle <- Oracle %>% 
+  mutate(WRKD.ENTITY = substr(Oracle$WD_COFT,1,3),
+         Hours = as.numeric(Hours),
+         Expense = as.numeric(Expense))
+Oracle <- Oracle%>%
+  group_by_at(c(1:13,16:33)) %>%
+  summarise(Hours = sum(Hours, na.rm = T),
+            Expense = sum(Expense,na.rm = T)) %>%
+  ungroup() %>%
+  distinct()
+#Bring in Payroll
+COA <- read.csv("~/MSH-MSQ-Payroll/Reference Tables/SiteCOA2.csv",stringsAsFactors = F,header = F,colClasses = c("character","character"))
+colnames(COA) <- c("Entity","PAYROLL")
+Oracle <- left_join(Oracle,COA,by=c("WRKD.ENTITY"="Entity"))
+
+#Check sum of hours by end date to make sure data follows proper pattern
+check <- Oracle %>% ungroup() %>% group_by(PAYROLL,End.Date) %>% summarise(Hours = sum(Hours)) %>%pivot_wider(id_cols = PAYROLL,values_from = Hours,names_from = End.Date)
+#Save .rds
+saveRDS(Oracle,file = "J:/deans/Presidents/SixSigma/MSHS Productivity/Productivity/Analysis/FEMA Reimbursement/MSHS-FEMA-Reimbursement/Reference Tables/data_MSH_MSQ_oracle.rds")
---
+++ Raw-Refresh/MSH_MSQ_Raw_Refresh.R
@@ -0,0 +1,50 @@
+library(dplyr)
+library(readxl)
+library(openxlsx)
+
+dir <- "~/MSH-MSQ-Payroll/"
+setwd(dir)
+
+##MSQ##
+#List files from MSQ Raw folder
+setwd(paste0(dir,"MSQ Raw/"))
+folderMSQ <- paste0(dir,"MSQ Raw/")     
+MSQ_file_list <- list.files(path=folderMSQ, pattern="*.txt")
+#Read files in MSQ Raw as csv
+MSQlist = lapply(MSQ_file_list, function(x)read.csv(x, sep = ";", header=T, stringsAsFactors = F,colClasses = rep("character",18)))
+#Bind all MSQ files and assign their site = "MSQ"
+MSQ = do.call("rbind", MSQlist)
+#Remove Duplicate rows
+MSQ <- MSQ %>% distinct()
+
+##MSH##
+#List files from MSH Raw folder
+setwd(paste0(dir,"MSH Raw/"))
+folderMSH <- paste0(dir,"MSH Raw/")   
+MSH_file_list <- list.files(path=folderMSH, pattern="*.txt")                              
+#Read files in MSH Raw as csv    
+MSHlist = lapply(MSH_file_list, function(x)read.csv(x, sep = ";", header=T, stringsAsFactors = F)) 
+#Bind all MSH files and assign their site = "MSH"
+MSH = do.call("rbind", MSHlist)
+#remove duplicate rows
+MSH <- MSH %>% distinct()
+
+#Bind MSH and MSQ
+MSHQ <- rbind(MSH,MSQ)
+
+#Assign correct Payroll Site
+COA <- read.csv("~/MSH-MSQ-Payroll/Reference Tables/SiteCOA.csv",stringsAsFactors = F)
+MSHQ <- left_join(MSHQ,COA,by = c("DPT.WRKD"="Legacy.CostCenter.Fund")) %>%
+  select(c(1:17,20))
+na <- filter(MSHQ,is.na(PAYROLL))
+na <- na %>% 
+  mutate(PAYROLL = 
+    case_when(substr(na$DPT.WRKD,1,3) == "013" ~ "MSQ",
+              substr(na$DPT.WRKD,1,3) != "013" ~ "MSH")
+  )
+
+#Form final datafram
+data_MSH_MSQ <- rbind(filter(MSHQ,!is.na(PAYROLL)),na)
+
+setwd("J:/deans/Presidents/SixSigma/MSHS Productivity/Productivity/Analysis/FEMA Reimbursement/MSHS-FEMA-Reimbursement/Reference Tables")
+saveRDS(data_MSH_MSQ,file = "data_MSH_MSQ.rds")
