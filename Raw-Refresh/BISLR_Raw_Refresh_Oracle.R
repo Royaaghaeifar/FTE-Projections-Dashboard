584e5ff0e5aa1c1e9190ba12b6c6aca732e33f1f
Miesle <matthew.miesle@mssm.edu>
2021-07-13 14:49:13 -0400

    added DUS removal handling

--- Raw-Refresh/BISLR_Raw_Refresh_Oracle.R
+++ Raw-Refresh/BISLR_Raw_Refresh_Oracle.R
@@ -4,9 +4,9 @@ library(here)
 
 # Constants ---------------------------------------------------------------
 #Start date is 1 day after the end of the last Premier Distribution
-filter_start_dates <- c("2021-02-28", "2021-03-28")
+filter_start_dates <- c("2021-02-28", "2021-03-28", "2021-04-25")
 #End date is 1 week after the end of the current Premier Distribution
-filter_end_dates <- c("2021-04-03", "2021-05-01")
+filter_end_dates <- c("2021-04-03", "2021-05-01", "2021-05-29")
 #Premier Distribution ex: 2/28/21- 3/27/21, 3/28/21 - 4/24/21
 
 #Names of the weekly paycyle names in the payroll name column in data files
@@ -20,7 +20,7 @@ dir_files <- paste0(here(), "/BISLR Oracle/BISLR Oracle/")
 #Update legacy raw refresh to include the deleted weekly cycle 2/28/21 - 3/6/21
 
 # Import Data -------------------------------------------------------------
-#Sort files based on index (oldest to newest) must match the 
+#Sort files based on index (oldest to newest) must match the
   #same order are filter end and start date
 sort_import_files <- function(dir_data){
   #Compiling data on files
@@ -92,7 +92,42 @@ data_BISLR <- lapply(data_BISLR, function(x) delete_weekly(x, weekly_pc))
 data_BISLR <- do.call(rbind, data_BISLR)
 
 #Removing duplicates
-data_BISLR <- data_BISLR %>% mutate(PAYROLL == "BISLR") %>% distinct()
+data_BISLR <- data_BISLR %>% mutate(PAYROLL = "BISLR") %>% distinct()
+# need to update the Payroll name to distinguish between different sites
+
+data_BISLR <- data_BISLR %>%
+  mutate(Position.Code.Description = case_when(
+    (Department.IdWHERE.Worked == 407210340412756 & Employee.Name == "CHIANG, JACQUELINE PE") ~ "DUS_REMOVE",
+    (Department.IdWHERE.Worked == 414000040312763 & Employee.Name == "BESTREICH, ERIN S") ~ "DUS_REMOVE",
+    (Department.IdWHERE.Worked == 414000040312755 & Employee.Name == "CHIANG, JACQUELINE PE") ~ "DUS_REMOVE",
+    (Department.IdWHERE.Worked == 414000040312755 & Employee.Name == "CRAIG, BRITTANY PIERCE") ~ "DUS_REMOVE",
+    (Department.IdWHERE.Worked == 414000040312756 & Employee.Name == "DELAPENHA, SANDRA ELAINE") ~ "DUS_REMOVE",
+    (Department.IdWHERE.Worked == 414000040312755 & Employee.Name == "GANZ, CINDY MARIE") ~ "DUS_REMOVE",
+    (Department.IdWHERE.Worked == 407210340412756 & Employee.Name == "MEEHAN, JENNIFER JOYCE") ~ "DUS_REMOVE",
+    (Department.IdWHERE.Worked == 414000040312702 & Employee.Name == "MEEHAN, JENNIFER JOYCE") ~ "DUS_REMOVE",
+    (Department.IdWHERE.Worked == 407210340412756 & Employee.Name == "OKAY, DEVIN JOSEPH") ~ "DUS_REMOVE",
+    (Department.IdWHERE.Worked == 407210340412756 & Employee.Name == "ROBBINS, STEPHANIE") ~ "DUS_REMOVE",
+    (Department.IdWHERE.Worked == 414000040312763 & Employee.Name == "WALKER, THERESA L") ~ "DUS_REMOVE",
+    (Department.IdWHERE.Worked == 414000040312763 & Employee.Name == "WALKER, THERESA L (Lauren)") ~ "DUS_REMOVE",
+    TRUE ~ Position.Code.Description)
+  )
+
+data_BISLR <- data_BISLR %>%
+  mutate(Job.Code = case_when(
+    (Department.IdWHERE.Worked == 407210340412756 & Employee.Name == "CHIANG, JACQUELINE PE") ~ "DUS_RMV",
+    (Department.IdWHERE.Worked == 414000040312763 & Employee.Name == "BESTREICH, ERIN S") ~ "DUS_RMV",
+    (Department.IdWHERE.Worked == 414000040312755 & Employee.Name == "CHIANG, JACQUELINE PE") ~ "DUS_RMV",
+    (Department.IdWHERE.Worked == 414000040312755 & Employee.Name == "CRAIG, BRITTANY PIERCE") ~ "DUS_RMV",
+    (Department.IdWHERE.Worked == 414000040312756 & Employee.Name == "DELAPENHA, SANDRA ELAINE") ~ "DUS_RMV",
+    (Department.IdWHERE.Worked == 414000040312755 & Employee.Name == "GANZ, CINDY MARIE") ~ "DUS_RMV",
+    (Department.IdWHERE.Worked == 407210340412756 & Employee.Name == "MEEHAN, JENNIFER JOYCE") ~ "DUS_RMV",
+    (Department.IdWHERE.Worked == 414000040312702 & Employee.Name == "MEEHAN, JENNIFER JOYCE") ~ "DUS_RMV",
+    (Department.IdWHERE.Worked == 407210340412756 & Employee.Name == "OKAY, DEVIN JOSEPH") ~ "DUS_RMV",
+    (Department.IdWHERE.Worked == 407210340412756 & Employee.Name == "ROBBINS, STEPHANIE") ~ "DUS_RMV",
+    (Department.IdWHERE.Worked == 414000040312763 & Employee.Name == "WALKER, THERESA L") ~ "DUS_RMV",
+    (Department.IdWHERE.Worked == 414000040312763 & Employee.Name == "WALKER, THERESA L (Lauren)") ~ "DUS_RMV",
+    TRUE ~ Job.Code)
+  )
 
 # Save RDS ----------------------------------------------------------------
 write_rds(data_BISLR,
