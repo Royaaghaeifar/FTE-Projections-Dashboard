394f8028e92d4dc6bf21325939d561cf28428e66
Weber <anjelica.weber@mssm.edu>
2022-01-24 14:18:57 -0500

    Updating BISLR pay period dates

--- Raw-Refresh/BISLR_Raw_Refresh_Oracle.R
+++ Raw-Refresh/BISLR_Raw_Refresh_Oracle.R
@@ -5,10 +5,12 @@ library(here)
 # Constants ---------------------------------------------------------------
 #Start date is 1 day after the end of the last Premier Distribution
 filter_start_dates <- c("2021-02-28", "2021-03-28", "2021-04-25", "2021-05-23",
-                        "2021-06-20", "2021-08-01", "2021-08-29")
+                        "2021-06-20", "2021-08-01", "2021-08-29", "2021-09-26",
+                        "2021-10-24", "2021-11-21")
 #End date is 1 week after the end of the current Premier Distribution
 filter_end_dates <- c("2021-04-03", "2021-05-01", "2021-05-29", "2021-06-26",
-                      "2021-08-07","2021-09-04", "2021-10-02")
+                      "2021-08-07","2021-09-04", "2021-10-02",  "2021-10-30",
+                      "2021-11-27", "2022-01-08")
 #Premier Distribution ex: 2/28/21- 3/27/21, 3/28/21 - 4/24/21
 # First weekly cycle for each BISLR Oracle file (ex. 3/28 - 4/3) is deleted
 # using delete_weekly() custom function (see function below)
